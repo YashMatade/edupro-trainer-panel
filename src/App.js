431a8b73e61619a902fc2c5b4d71c6296037747b
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2022-10-19 18:31:36 +0530

    .

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/courses" element={<Courses />} />
         </Routes>
         <Footer />
